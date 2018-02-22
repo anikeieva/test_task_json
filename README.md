55d084596d5663cea02c014f5ddf08e3b7c4c9b2
anikeieva <alyonaoooooooo@gmail.com>
2018-02-22 14:34:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# test_task_json_vue
+
+This is a test project. In it was realized outputing test json data
+to the HTML-table and live search by separate fields. Also there is
+counting total currency(fourth table's field).
+
+Project was created with the help of Vue.js, vue-cli. Also I worked with:
+Node.js, Webpack, Sass, ES6. 
+
+Follow the link to see a page of project (at GitHub Pages):
+https://anikeieva.github.io/test_task_json_vue/
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
---
+++ src/main.js
@@ -0,0 +1,56 @@
+import Vue from 'vue';
+import jsonArray from '../data/test.json'
+import '../sass/main.sass'
+
+let jsonStr = JSON.stringify(jsonArray);
+let dataArray = JSON.parse(jsonStr);
+
+new Vue({
+  el: "#app",
+  data: {
+    array: dataArray,
+    keysArray: ['id', 'name', 'location', 'currency'],
+    user_message: {
+      id: '',
+      name: '',
+      location: '',
+      currency: ''
+    },
+    filteredArray: dataArray,
+  },
+  methods: {
+    filter: function(e) {
+      let result = this.array.filter(user => {
+        const key = e.target.id;
+        let tableData;
+        let searchingData;
+        if (typeof user[key] == "number") {
+          tableData = user[key].toString();
+          searchingData = this.user_message[key].toString();
+        } else {
+          tableData = user[key].toLowerCase();
+          searchingData = this.user_message[key].toLowerCase();
+        }
+
+        for (var prop in this.user_message) {
+          if(prop !== key) {
+            this.user_message[prop] = "";
+          }
+        }
+
+        return tableData.match(searchingData);
+      });
+      this.filteredArray = result;
+      return result;
+    }
+  },
+  computed: {
+    totalCurrency: function() {
+      let resultSum = 0;
+      this.filteredArray.filter(user => {
+        resultSum += parseInt(user.currency);
+      });
+      return resultSum;
+    }
+  }
+});
